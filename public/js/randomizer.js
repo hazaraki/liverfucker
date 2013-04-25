f5ee067a02bfb1306ed1cc89eba342ab8fac8e82
reesef <reesef@gmail.com>
2013-04-25 00:41:16 -0700

    Work on js

--- public/js/randomizer.js
+++ public/js/randomizer.js
@@ -1,8 +1,12 @@
+  function answer() {  
+    
+    var response = new Array (); 
+	  response[0] = "test-0";
+	  response[1] = "test-1";
+	  response[2] = "test-2";
 
+	var i = Math.floor(Math.random()*3);
 
-var response = new Array (); 
-response[0] = "test-0";
-response[1] = "test-1";
-response[2] = "test-2";
+	document.getElementByID("response").innerText = response[i]; 
 
-var i = Math.floor(Math.random()*3)
+	};
