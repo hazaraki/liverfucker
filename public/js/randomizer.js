ca73bcb99054ba5471c8faf2b2c4b36f29504daa
reesef <reesef@gmail.com>
2013-04-28 18:04:40 -0700

    Move JS to external file

--- public/js/randomizer.js
+++ public/js/randomizer.js
@@ -1,12 +1,17 @@
-  function answer() {  
+ $(document).ready(function() {
+
     
+
     var response = new Array (); 
 	  response[0] = "test-0";
 	  response[1] = "test-1";
 	  response[2] = "test-2";
+	  
+	var i = Math.floor(Math.random()*3)
 
-	var i = Math.floor(Math.random()*3);
 
+function answer() {
 	document.getElementByID("response").innerText = response[i]; 
+};
 
-	};
+});
