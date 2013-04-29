3229025d67c685d0d6bb1cfc890afd9ddc32010b
reesef <reesef@gmail.com>
2013-04-28 18:37:29 -0700

    make the randomizer work

--- public/js/randomizer.js
+++ public/js/randomizer.js
@@ -1,17 +1,9 @@
-$(document).ready(function() {
-
-    
-
-    var response = new Array (); 
+var response = new Array (); 
 	  response[0] = "test-0";
 	  response[1] = "test-1";
 	  response[2] = "test-2";
-	  
-	var i = Math.floor(Math.random()*3)
-
 
 function answer() {
-	document.getElementByID("response").innerText = response[i]; 
+	var i = Math.floor(Math.random()*3)
+	document.getElementById("response").innerText = response[i]; 
 };
-
-});
